1cbae6436b04cb9659297406ed8f594df6997932
Bulduk <sbulduk@TASLOG.local>
2024-05-13 11:41:29 +0200

    Authentication completed.
    Test was done with a working authentication data.

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-from dotenv import load_dotenv
+import sys
+sys.dont_write_bytecode=True
 from abc import ABC,abstractmethod
 from apis.authentication import Authentication
-
-load_dotenv()
-
 
 class Main(ABC):
     @abstractmethod
     def Run():
         auth=Authentication()
-        authResult=auth.LoginUser("sbulduk","Sbulduk2023!")
-        if(authResult['success']==True):
-            print(f"Login Successful")
-            print(f"Access Token: {authResult['access_token']}")
-        else:
-            print(f"Login Failed")
-            print(f"Message: {authResult['message']}")
-
-        # auth=Authentication("sbulduk","Sbulduk2023!")
-        # if(auth.LoginUser()):
-        #     print("Login Successful")
-        # else:
-        #     print("Login Failed")
+        authResult=auth.LoginUser("KK_GEBRTASKIN","123")
 
 if(__name__=="__main__"):
     Main.Run()
